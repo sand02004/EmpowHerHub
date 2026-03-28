54561339771ba3b8b4a28aea5295a70fbcae7297
sand02004 <s.mugwaneza@alustudent.com>
2026-03-28 14:05:12 +0200

    fixing paths

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   app.setGlobalPrefix('api/v1');
 
   // --- SWAGGER SETUP ---
